5931d036bb674402b9dacfa240baa353fb34179a
キムラ　ナオト <kim.u.k7526@gmail.com>
2022-02-02 20:47:46 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//ヘッダーの色変更
+$(function(){
+  var $win = $(window),
+      $mv = $('.mainvisual'),
+      $header = $('.header-area'),
+      mvHeight = $mv.outerHeight(),
+      editClass = 'black';
+
+  $win.on('load scroll', function(){
+    var value = $(this).scrollTop();
+    
+    if( value > mvHeight){
+      $header.addClass(editClass);
+    }else{
+      $header.removeClass(editClass);
+    };
+
+  });
+})
+
+//スライダー
+$(function(){
+  $('.slick-slider').slick({
+    autoplay :true,
+    autoplayspeed: 2000,
+    fade: true,
+    speed: 1000,
+    cssEase: 'linear'
+  });
+});
+
+//ハンバーガーメニュー
+$(function(){
+  $('.burger').on('click', function(){
+    $('.layer, .burger, body').toggleClass('open');
+  });
+});
